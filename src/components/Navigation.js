c5bb1ffd26a64dd28bf8f3ead3bfaf55121e3626
Megi <deringmagdalena@gmail.com>
2018-04-23 22:00:40 +0200

    finished sign up section code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-export default () =>
+import * as routes from '../constants/routes';
+
+const Navigation = () =>
     <div>
         <ul>
-            <li>Profile</li>
+            <li><Link to={routes.SIGN_IN}>Sign in</Link></li>
+            <li><Link to={routes.SIGN_UP}>Sign up</Link></li>
+            <li><Link to={routes.LANDING}>Landing</Link></li>
+            <li><Link to={routes.HOME}>Home</Link></li>
+            <li><Link to={routes.ACCOUNT}>Account</Link></li>
         </ul>
-    </div>;
+        <p>Hello World, content is no changing</p>
+    </div>
 
+
+export default Navigation;
