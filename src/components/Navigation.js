842e94c899bbfe795f919235d741d37f9da938dd
Megi <deringmagdalena@gmail.com>
2018-05-02 23:34:15 +0200

    finished authentication process

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import AuthUserContext from './AuthUserContext';
 import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
-const Navigation = ({authUser}) =>
-    <div>{
-        authUser
-            ? <NavigationUserNonRegister/>
-            : <NavigationUserRegister/>
-    }
-    </div>
+const Navigation = () =>
+ <AuthUserContext.Consumer>
+ {authUser => authUser
+ ? <NavigationUserNonRegister/>
+ : <NavigationUserRegister/>
+}
+</AuthUserContext.Consumer>
 
 
 const NavigationUserNonRegister = () =>
