bb0c63a6a1860df13219cedd7eb1f3ee61a6a704
Natalia Kulas <nataliakulas@gmail.com>
2018-05-04 14:39:30 +0200

    corrections after merge

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,32 +2,34 @@ import * as firebase from 'firebase';
 // copied from firebase dashboard
 
 const prodConfig = {
-    apiKey: "AIzaSyD_Vpbkg3E3mXrJ3YWutc8v_NMRbbAvQ_k",
-    authDomain: "iggi-webapp.firebaseapp.com",
-    databaseURL: "https://iggi-webapp.firebaseio.com",
-    projectId: "iggi-webapp",
-    storageBucket: "iggi-webapp.appspot.com",
-    messagingSenderId: "5112353845"
+  apiKey: "AIzaSyD_Vpbkg3E3mXrJ3YWutc8v_NMRbbAvQ_k",
+  authDomain: "iggi-webapp.firebaseapp.com",
+  databaseURL: "https://iggi-webapp.firebaseio.com",
+  projectId: "iggi-webapp",
+  storageBucket: "iggi-webapp.appspot.com",
+  messagingSenderId: "5112353845"
 };
+
 // for production
 const devConfig = {
-    apiKey: "AIzaSyD_Vpbkg3E3mXrJ3YWutc8v_NMRbbAvQ_k",
-    authDomain: "iggi-webapp.firebaseapp.com",
-    databaseURL: "https://iggi-webapp.firebaseio.com",
-    projectId: "iggi-webapp",
-    storageBucket: "iggi-webapp.appspot.com",
-    messagingSenderId: "5112353845"
+  apiKey: "AIzaSyD_Vpbkg3E3mXrJ3YWutc8v_NMRbbAvQ_k",
+  authDomain: "iggi-webapp.firebaseapp.com",
+  databaseURL: "https://iggi-webapp.firebaseio.com",
+  projectId: "iggi-webapp",
+  storageBucket: "iggi-webapp.appspot.com",
+  messagingSenderId: "5112353845"
 };
 
 const config = process.env.NODE_ENV === 'production'
-? prodConfig
-    : devConfig;
+  ? prodConfig
+  : devConfig;
 
-if(!firebase.apps.length) {
-    firebase.initializeApp(config);
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
 }
 
 const auth = firebase.auth();
+
 export {
-    auth,
+  auth,
 };
