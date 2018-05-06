5b9dd70498e3c9b09eacd77b94cd92b2bcfdfd43
Magdalena Dering <deringmagdalena@gmail.com>
2018-05-06 14:54:24 +0200

    added selectors for all types of food

--- src/pages/profile/create.js
+++ src/pages/profile/create.js
@@ -60,18 +60,72 @@ class CreateProfile extends React.Component {
     activeAlcohol: false
   });
 
+   activateGrains = () => this.setState({
+    activeAll: false,
+    activeFruits: false,
+    activeVegetables: false,
+    activeGrains: true,
+    activeDiary: false,
+    activeSpices: false,
+    activeDrinks: false,
+    activeAlcohol: false
+  });
+
+    activateDiary = () => this.setState({
+    activeAll: false,
+    activeFruits: false,
+    activeVegetables: false,
+    activeGrains: false,
+    activeDiary: true,
+    activeSpices: false,
+    activeDrinks: false,
+    activeAlcohol: false
+  });
+
+    activateSpices = () => this.setState({
+    activeAll: false,
+    activeFruits: false,
+    activeVegetables: false,
+    activeGrains: false,
+    activeDiary: false,
+    activeSpices: true,
+    activeDrinks: false,
+    activeAlcohol: false
+  });
+
+    activateDrinks = () => this.setState({
+    activeAll: false,
+    activeFruits: false,
+    activeVegetables: false,
+    activeGrains: false,
+    activeDiary: false,
+    activeSpices: false,
+    activeDrinks: true,
+    activeAlcohol: false
+  });
+
+    activateAlcohol = () => this.setState({
+    activeAll: false,
+    activeFruits: false,
+    activeVegetables: false,
+    activeGrains: false,
+    activeDiary: false,
+    activeSpices: false,
+    activeDrinks: false,
+    activeAlcohol: true
+  });
+
 
   componentDidMount() {
     db.getProducts().then(snap => {
         const products = [];
-
-        snap.forEach(item => {
+        
+          snap.forEach(item => {
           let product = item.val();
           product.name = item.key;
 
           products.push(product)
-        });
-        console.log(products);
+        });     
 
         if (products.length > 0) {
           this.setState(() => ({hasProducts: true, products: products}))
@@ -92,11 +146,11 @@ class CreateProfile extends React.Component {
                     <li onClick={this.activateAll} className={`${this.state.activeAll ? "active" : ""}`}>All</li>
                     <li onClick={this.activateFruits} className={`${this.state.activeFruits ? "active" : ""}`}>Fruits</li>
                     <li onClick={this.activateVegetables} className={`${this.state.activeVegetables ? "active" : ""}`}>Vegetables</li>
-                    <li>Grains</li>
-                    <li>Diary</li>
-                    <li>Spices</li>
-                    <li>Drinks</li>
-                    <li>Alcohol</li>
+                    <li onClick={this.activateGrains} className = {`${this.state.activeGrains ? "active" : ""}`}>Grains</li>
+                    <li onClick={this.activateDiary} className = {`${this.state.activeDiary ? "active" : ""}`}>Diary</li>
+                    <li onClick={this.activateSpices} className = {`${this.state.activeSpices ? "active" : ""}`}>Spices</li>
+                    <li onClick={this.activateDrinks} className = {`${this.state.activeDrinks ? "active" : ""}`}>Drinks</li>
+                    <li onClick={this.activateAlcohol} className = {`${this.state.activeAlcohol ? "active" : ""}`}>Alcohol</li>        
                   </ul>
 
                   {this.state.activeAll && this.state.products.map(product => {
@@ -112,6 +166,46 @@ class CreateProfile extends React.Component {
                       )
                     }
                   })}
+
+                    {this.state.activeVegetables && this.state.products.map(product => {
+                    if (product.group === 'vegetables') {
+                      return (
+                        <div className="name">{product.name}</div>
+                      )
+                    }
+                  })}
+
+                    {this.state.activeGrains && this.state.products.map(product => {
+                      if (product.group === 'grains'){
+                        return(
+                          <div className="name">{product.name}</div>
+                          ) 
+                         }
+                      })}
+
+                    {this.state.activeDiary && this.state.products.map(product => {
+                      if (product.group === 'diary'){
+                        return (
+                          <div className="name">{product.name}</div>
+                          )
+                      }
+                    })}
+
+                    {this.state.activeSpices && this.state.products.map(product => {
+                      if (product.group === 'spices'){
+                        return (
+                          <div className="name">{product.name}</div>
+                          )
+                      }
+                    })}
+                      {this.state.activeAlcohol && this.state.products.map(product => {
+                      if (product.group === 'alcohol'){
+                        return (
+                          <div className="name">{product.name}</div>
+                          )
+                      }
+                    })}
+
                 </div>
                 : null}
             </Col>
